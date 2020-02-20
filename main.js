087a75678e564a861909727380540a3b00edfc48
Piotr <piotr.romatowski@gmail.com>
2020-02-20 18:08:08 +0100

    prompt-kalkulator

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ let guzikplus = document.getElementById("dod");
 let guzikminus = document.getElementById("min");
 let guzikmnoz = document.getElementById("mno");
 let ramka = document.getElementById("ekran");
+let dana1;
+let dana2;
 
 
-guzik.onclick = function dane (dana1,dana2) {
+guzik.onclick = function dane () {
     dana1 = prompt("Podaj 1 liczbę");
     dana2 = prompt("Podaj 2 liczbę");
     par1.textContent='Liczba 1 to: '+ dana1;
@@ -16,11 +18,24 @@ guzik.onclick = function dane (dana1,dana2) {
 
 
 
-guzikplus.onclick = function dodawanie (x1,x2) {
-  x1 = par1.dana1;
-  x2 = par2.dana2;
-  ramka.textContent = (x1+x2);
-  
+guzikplus.onclick = function dodawanie () {
+  dana1=dana1/1;
+  dana2=dana2/1;
+  let result = dana1 + dana2;
+  ramka.textContent = result;
 }
 
+guzikminus.onclick = function odejmowanie () {
+  dana1=dana1/1;
+  dana2=dana2/1;
+  let result = dana1 - dana2;
+  ramka.textContent = result;
+}
+
+guzikmnoz.onclick = function mnozenie () {
+  dana1=dana1/1;
+  dana2=dana2/1;
+  let result = dana1 * dana2;
+  ramka.textContent = result;
+}
 
